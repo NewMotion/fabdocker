fb2c4a8d2293b83a4285c1e7db876bfcb2802bf6
g.assies <gertjan.assies@gmail.com>
2019-12-18 15:11:01 +0100

    Upped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='fabdocker',
-    version='0.3.5',
+    version='0.3.6',
     url='https://github.com/newmotion/fabdocker',
     license='MIT',
     author='Daan Debie',
